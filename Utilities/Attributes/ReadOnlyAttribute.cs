a1aa8011f36bdeee51749a16adf0c84cc9a41293
redHurt96 <62645756+redHurt96@users.noreply.github.com>
2021-09-08 23:35:16 +0300

    Update ReadOnlyAttribute.cs

--- Utilities/Attributes/ReadOnlyAttribute.cs
+++ Utilities/Attributes/ReadOnlyAttribute.cs
@@ -1,26 +1,29 @@
 using UnityEditor;
 using UnityEngine;
 
-public class ReadOnlyAttribute : PropertyAttribute
+namespace RH.Utilities.Attributes
 {
- 
-}
- 
-[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
-public class ReadOnlyDrawer : PropertyDrawer
-{
-    public override float GetPropertyHeight(SerializedProperty property,
-        GUIContent label)
+    public class ReadOnlyAttribute : PropertyAttribute
     {
-        return EditorGUI.GetPropertyHeight(property, label, true);
+
     }
- 
-    public override void OnGUI(Rect position,
-        SerializedProperty property,
-        GUIContent label)
+
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
     {
-        GUI.enabled = false;
-        EditorGUI.PropertyField(position, property, label, true);
-        GUI.enabled = true;
+        public override float GetPropertyHeight(SerializedProperty property,
+            GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        public override void OnGUI(Rect position,
+            SerializedProperty property,
+            GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
     }
 }
